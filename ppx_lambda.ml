f187dfefd4245abed2321f84f5c0d7d2a3ddf2ee
Rizo Isrof <rizo@odis.io>
2014-08-26 20:01:36 +0100

    Added support for n-ary lambdas.

--- ppx_lambda.ml
+++ ppx_lambda.ml
@@ -7,27 +7,58 @@ open Longident
 open Location
 
 
+let make_1_ary_fun var loc expr =
+  let var_pat = Pat.var (mkloc var loc) in
+  Exp.fun_ "" None var_pat expr
+
+
+let make_0_ary_fun loc expr =
+  let unit_id = mkloc (Lident "()") loc in
+  let unit_pat = Pat.construct unit_id None in
+  Exp.fun_ "" None unit_pat expr
+
+
+let rec make_n_ary_fun args body =
+  match args with
+  | ((l0, {pexp_desc = Pexp_ident {txt = Lident arg; loc = arg_loc}})::[])
+    -> make_1_ary_fun arg arg_loc body
+
+  | ((l0, {pexp_desc = Pexp_ident {txt = Lident arg; loc = arg_loc}})::rest)
+    -> make_1_ary_fun arg arg_loc (make_n_ary_fun rest body)
+
+  | [] -> make_0_ary_fun Location.none body
+
+  | _ -> raise (Failure "Bad arguments for function construction.")
+
+
 let lambda_mapper argv =
   let super = default_mapper in
   let lambda_expr this e =
     match e.pexp_desc with
     (*
-     * Unit mapper: `() => e0` -> `fun () -> e0`
+     * 0-ary lambda mapper: `() => e0` -> `fun () -> e0`
      *)
     | Pexp_apply ({pexp_desc = Pexp_ident {txt = Lident "=>"}},
-       [("", {pexp_desc = Pexp_construct ({txt = Lident "()"} as id, None)});
+       [("", {pexp_desc =
+          Pexp_construct ({txt = Lident "()"; loc = unit_loc}, None)});
         ("", e0)]) ->
-      let unit_pat = Pat.construct id None in
-      Exp.fun_ "" None unit_pat e0
+      make_0_ary_fun unit_loc e0
 
     (*
-     * Apply mapper: `arg0 => e0` -> `fun arg0 -> e0`
+     * 1-ary lambda mapper: `arg0 => e0` -> `fun arg0 -> e0`
      *)
     | Pexp_apply ({pexp_desc = Pexp_ident {txt = Lident "=>"}},
         [("", {pexp_desc = Pexp_ident {txt = Lident arg0; loc = arg0_loc}});
          ("", e0)]) ->
-      let arg0_pat = Pat.var (mkloc arg0 arg0_loc) in
-      Exp.fun_ "" None arg0_pat e0
+      make_1_ary_fun arg0 arg0_loc e0
+
+    (*
+     * n-ary lambda mapper: `arg0 ... argN => e0` -> `fun arg0 ... argN -> e0`
+     *)
+    | Pexp_apply ({pexp_desc = Pexp_ident {txt = Lident "=>"}},
+        [("", {pexp_desc = Pexp_apply (arg0_no_lab, arg_rest)});
+         ("", e0)]) ->
+      make_n_ary_fun (("", arg0_no_lab)::arg_rest) e0
 
     (* default expr *)
     | _ -> super.expr this e
--- test_ppx_lambda.ml
+++ test_ppx_lambda.ml
@@ -8,9 +8,10 @@ let inc = x => x + 1
 let ignore = (x) => 42
 let hello = name => "Hello, " ^ name ^ "!"
 
-let seq_lambda1 = x => x; 42
-let seq_lambda2 = (x => x); 42
+let seq_lambda1 = x => x, 42
+let seq_lambda2 = x => (x, 42)
 
+let three_sum = x y z => x + y + z
 
 let test () =
     assert (obtain_number () = 42);
@@ -24,17 +25,14 @@ let test () =
     assert (hello "Bob" = "Hello, Bob!");
     assert (((x => x + 1) 1) = 2);
     assert ((List.map (x => x * x) [1; 2; 3]) = [1; 4; 9]);
-    assert (seq_lambda1 5 = 42);
-    assert (seq_lambda2 5 = 42)
+    let (f, n) = seq_lambda1 in begin
+        assert (f 5 = 5);
+        assert (n = 42) 
+    end;
+    assert (seq_lambda2 0 = (0, 42));
+    assert (three_sum 1 2 3 = 6);
+    assert ((x y => x + y) 2 3 = 5)
 
 
-let demo () =
-    print_endline ("obtain_number () = " ^ string_of_int (obtain_number ()));
-    print_endline ("inc 1 = " ^ string_of_int (inc 1));
-    print_endline ("ignore 5 = " ^ string_of_int (ignore 5));
-    print_endline ("hello \"Bob\" = " ^ hello "Bob")
+let () = test ()
 
-
-let () =
-    test ();
-    demo ()
